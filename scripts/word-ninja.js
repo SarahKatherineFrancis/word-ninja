a0dec47a2fdce176ca52f93ae1f9d57322fa71b0
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-05-09 14:40:18 +0200

    added event listener and key actions

--- scripts/word-ninja.js
+++ scripts/word-ninja.js
@@ -2,7 +2,19 @@ const letters = document.querySelectorAll(".scoreboard-letter");
 const loadingDiv = document.querySelector(".info-bar");
 
 async function init() {
-  document.addEventListener("keydown", e);
+  document.addEventListener("keydown", function handleKeyPress(event) {
+    const action = event.key;
+
+    if (action === "Enter") {
+      commit();
+    } else if (action === "Backspace") {
+      backspace();
+    } else if (isLetter(action)) {
+      addLetter(action.toUpperCase());
+    } else {
+      // do nothing 
+    }
+  });
 }
 
 init();
