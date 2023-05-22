9e53ffe6f92bb16a662e91d8eca1eb2c6137d6fe
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-05-22 15:08:08 +0200

    update scoreboard based on guess comparison

--- scripts/word-ninja.js
+++ scripts/word-ninja.js
@@ -13,12 +13,14 @@ async function init() {
   let currentGuess = "";
   let currentRow = 0;
 
+  // Fetch the word of the day from the API
   const res = await fetch("https://words.dev-apis.com/word-of-the-day");
   const resObj = await res.json();
   const word = resObj.word.toUpperCase();
-  setLoading(false);
+  const wordParts = word.split("");
 
-  console.log(word);
+  // Set loading state to false
+  setLoading(false);
 
   // Function to add a letter to the current guess
   function addLetter(letter) {
@@ -35,15 +37,36 @@ async function init() {
       letter;
   }
 
+  // Function to handle the commit action
   async function commit() {
     if (currentGuess.length !== ANSWER_LENGTH) {
       // do nothing
       return;
     }
+
+    const guessParts = currentGuess.split("");
+
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      if (guessParts[i] === wordParts[i]) {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("correct");
+      }
+    }
+
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      if (guessParts[i] === wordParts[i]) {
+        // do nothing
+      } else if (wordParts.includes(guessParts[i])) {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("close");
+      } else {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("wrong");
+      }
+    }
+
     currentRow++;
     currentGuess = "";
   }
 
+  // Function to handle the backspace action
   function backspace() {
     currentGuess = currentGuess.substring(0, currentGuess.length - 1);
     letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = "";
@@ -72,6 +95,7 @@ function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 }
 
+// Function to toggle the loading state
 function setLoading(isLoading) {
   loadingDiv.classList.toggle("hidden", !isLoading);
 }
